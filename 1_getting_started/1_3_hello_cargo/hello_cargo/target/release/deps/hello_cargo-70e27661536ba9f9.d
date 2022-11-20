9075c82d215f23ff6675c36a3463b87a61effbc4
SATO-Hiroki-tbn <hiroki_sato@tbn.co.jp>
2022-11-20 13:53:36 +0900

    1.3.Hello, Cargo: cargo build --releaseを実行

---
+++ 1_getting_started/1_3_hello_cargo/hello_cargo/target/release/deps/hello_cargo-70e27661536ba9f9.d
@@ -0,0 +1,5 @@
+/Users/hirokitomono/work/projects/private/rust_tutorial/1_getting_started/1_3_hello_cargo/hello_cargo/target/release/deps/hello_cargo-70e27661536ba9f9: src/main.rs
+
+/Users/hirokitomono/work/projects/private/rust_tutorial/1_getting_started/1_3_hello_cargo/hello_cargo/target/release/deps/hello_cargo-70e27661536ba9f9.d: src/main.rs
+
+src/main.rs:
