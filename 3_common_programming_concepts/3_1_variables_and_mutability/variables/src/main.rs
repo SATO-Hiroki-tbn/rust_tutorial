09b4aa71da238b04168958e2269d8875d229d8bd
SATO-Hiroki-tbn <hiroki_sato@tbn.co.jp>
2022-11-23 13:09:39 +0900

    3.1. Variables and Mutability: constで定数を宣言

--- 3_common_programming_concepts/3_1_variables_and_mutability/variables/src/main.rs
+++ 3_common_programming_concepts/3_1_variables_and_mutability/variables/src/main.rs
@@ -3,4 +3,7 @@ fn main() {
     println!("The value of x is: {x}");
     x = 6;
     println!("The value of x is: {x}");
+    // const
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("The const value of THREE_HOURS_IN_SECONDS is: {THREE_HOURS_IN_SECONDS}");
 }
