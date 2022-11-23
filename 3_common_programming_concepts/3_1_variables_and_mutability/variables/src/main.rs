50973de6c3070b5e8315bb9fe387fa9a1fefc976
SATO-Hiroki-tbn <hiroki_sato@tbn.co.jp>
2022-11-23 13:21:18 +0900

    3.1. Variables and Mutability: shadowingの確認

--- 3_common_programming_concepts/3_1_variables_and_mutability/variables/src/main.rs
+++ 3_common_programming_concepts/3_1_variables_and_mutability/variables/src/main.rs
@@ -6,4 +6,22 @@ fn main() {
     // const
     const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
     println!("The const value of THREE_HOURS_IN_SECONDS is: {THREE_HOURS_IN_SECONDS}");
+    // shadowing 1
+    // letを使い同じ変数名を変換する.
+    // mutは同じ変数の値を変える.let(shadowing)は別の変数を定義することになる.
+    let y = 5;
+    let y = y + 1;
+    {
+        let y = y * 2;
+        println!("The value of y in the inner scope is: {y}"); // 12
+    }
+    println!("The value of y is: {y}"); // 6
+    // shadowing 2
+    // 別の型で宣言し直すことも可能.
+    let spaces = "   "; // 文字列型
+    let spaces = spaces.len(); // 数値型
+    println!("The value of spaces is: {spaces}"); // 3
+    // 以下はコンパイルエラー
+    // let mut spaces = "   ";
+    // spaces = spaces.len(); // 文字列型の変数に数値を入れようとするため
 }
